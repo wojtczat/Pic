bf9ac2996985e118ef6f25d521e6b0f6669d7209
Ori Almog <almogo@mcmaster.ca>
2016-09-17 10:45:14 -0400

    debug 2

--- app/pages/feed/feed.ts
+++ app/pages/feed/feed.ts
@@ -11,6 +11,24 @@ export class Feed {
 	pos: any = {};
 
 	constructor(public navCtrl: NavController, private data: DataService) {
-		this.data.getLocation(pos => this.pos = pos);
+		this.data.getLocation(pos => {
+			this.pos = pos;
+			console.log(pos);
+		});
+
+		let myValues = [
+			{
+				name: "Ori",
+				age: 20
+			},
+			{
+				name: "Jacoby",
+				age: 34
+			},
+			{
+				name: "Jacoby",
+				age: 34
+			},
+		];
 	}
 }
