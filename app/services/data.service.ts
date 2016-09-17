fa6ecf8107fc7cd14f72f365dea343c36bda0cce
JacobyJoukema <jacoby.joukema@gmail.com>
2016-09-17 14:58:07 -0400

    We Talk To Computer In NJ, NJ Talk to me WOWZA!

--- app/services/data.service.ts
+++ app/services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http, HTTP_PROVIDERS} from '@angular/http';
+import {Http, HTTP_PROVIDERS, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 import { Geolocation } from 'ionic-native';
@@ -9,11 +9,13 @@ import { File } from 'ionic-native';
 
 @Injectable()
 export class DataService {
+    
+    pos: any = {};
+    havePos: boolean = false;
 
 	base64Image: string;
 
 	constructor(private http: Http) {
-
 		//http.get('http://www.google.ca').subscribe(res => console.log(res));
 	}
 
@@ -37,9 +39,19 @@ export class DataService {
 			);
 			});
 			*/
-
-			
-
+            
+            if (this.havePos){
+                var data = {image:this.base64Image,longitude: this.pos.coords.longitude, latitude: this.pos.coords.latitude, time: Date.now()}
+                console.log(data);
+                var headers = new Headers();
+                 headers.append('Content-Type', 'application/json');
+                 var URL = "http://45.79.169.174/api/save";
+                
+                this.http.post(URL, data, {
+                    headers: headers
+                }).subscribe(res => console.log(res));
+            }
+           
 		}, (err) => {
 
 			console.log(err);
@@ -53,6 +65,10 @@ export class DataService {
 		console.log("Getting location");
 		Geolocation.getCurrentPosition().then(pos => {
 			console.log("OK!");
+            
+            this.havePos = true;
+            this.pos = pos;
+            
 			callback(pos);
 		}).catch(reason => {
 			console.log("ERROR");
